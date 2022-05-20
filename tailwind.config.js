6004d132cad4a219d79e7c80ba2c37a627337e17
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-20 20:21:08 +0300

    add files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      'lg': { 'max': '992px' },
+      'md': { 'max': '768px' },
+      'sm': { 'max': '480px' }
+    },
+    container: {
+      padding: '20px',
+      center: true,
+    },
+    extend: {
+      colors: {
+        lightblack: '#4D4244',
+        lightred: '#FF0D38',
+        darkred: '#D70026',
+        lightgray: '#747474',
+        darkgray: '#272727',
+      }
+    },
   },
   plugins: [],
 }
